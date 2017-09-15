28df7887ea5595c003f182cae04de4cc6ffd5a69
Leonardo Kanagusku <leonardo.kanagusku@gmail.com>
2017-09-15 16:57:55 -0400

    first commit - Starting arch reference, partially creating layers and TinkerPop dependency

---
+++ com.essencify/com.essencify.core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace com.essencify.core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
